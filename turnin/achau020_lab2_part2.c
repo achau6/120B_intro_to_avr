77c439302689f9823bbc95623a3d2de3f5e87c95
alan chau <achau020@wch136-22.cs.ucr.edu>
2021-04-07 19:42:30 -0700

    added lab section

--- turnin/achau020_lab2_part2.c
+++ turnin/achau020_lab2_part2.c
@@ -1,7 +1,7 @@
 /*	Author: achau020
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Lab Section: 21
+ *	Assignment: Lab 2  Exercise 2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
